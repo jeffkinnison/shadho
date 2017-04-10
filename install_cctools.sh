18e65fdce8b6a507480fa86e59421147a8565ccc
Jeff Kinnison <jeffdkinnison@gmail.com>
2017-04-10 16:42:26 -0400

    try again

--- install_cctools.sh
+++ install_cctools.sh
@@ -12,17 +12,15 @@ py2path=`dirname $py2path`
 py3path=`dirname $py3path`
 py3path=`dirname $py3path`
 
-if [ ! -z $py3path ]; then
-    if [ ! -f "$py3path/bin/2to3" ]; then
-        echo $'#!/usr/bin/env python3\nimport sys\nfrom lib2to3.main import main\n\nsys.exit(main("lib2to3.fixes"))' > "$py3path/bin/2to3"
-        chmod 755 "$py3path/bin/2to3"
-    fi
+if [ ! -f "$py3path/bin/2to3" ]; then
+    echo $'#!/usr/bin/env python3\nimport sys\nfrom lib2to3.main import main\n\nsys.exit(main("lib2to3.fixes"))' > "$py3path/bin/2to3"
+    chmod 755 "$py3path/bin/2to3"
+fi
 
-    if [ ! -f "$py3path/bin/python3-config" ]; then
-        sed -i "s@prefix_build=PREFIXBUILD@prefix_build=\"$py3path\"@" ./python3-config
-        mv ./python3-config $py3path
-        chmod 755 "$py3path/bin/python3-config"
-    fi
+if [ ! -f "$py3path/bin/python3-config" ]; then
+    sed -i "s@prefix_build=PREFIXBUILD@prefix_build=\"$py3path\"@" "./python3-config"
+    mv "./python3-config" $py3path
+    chmod 755 "$py3path/bin/python3-config"
 fi
 
 # Configure, make, and install

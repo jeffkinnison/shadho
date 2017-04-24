47f1ae80579f966949ad90ea2ff20e3bb61263ed
Jeff Kinnison <jeffdkinnison@gmail.com>
2017-04-24 16:10:24 -0400

    add README

--- install_cctools.sh
+++ install_cctools.sh
@@ -12,27 +12,9 @@ py2path=`dirname $py2path`
 py3path=`dirname $py3path`
 py3path=`dirname $py3path`
 
-if [ ! -f "$py3path/bin/2to3" ]; then
-    echo $'#!/usr/bin/env python3\nimport sys\nfrom lib2to3.main import main\n\nsys.exit(main("lib2to3.fixes"))' > "$py3path/bin/2to3"
-    chmod 755 "$py3path/bin/2to3"
-fi
-
-if [ ! -f "$py3path/bin/python3-config" ]; then
-    sed -i "s@prefix_build=PREFIXBUILD@prefix_build=\"$py3path\"@" "./python3-config"
-    sed -i "s@VERSION=\"3.5\"@VERSION=\"$TRAVIS_PYTHON_VERSION\"@" "./python3-config"
-    mv "./python3-config" "$py3path/bin"
-    chmod 755 "$py3path/bin/python3-config"
-fi
-
 # Configure, make, and install
 cd cctools
-libpy=`find /opt -name "libpython${TRAVIS_PYTHON_VERSION}m.so"`
-echo $libpy
-export CPATH="$py3path/include/python${TRAVIS_PYTHON_VERSION}m:$CPATH"
-export LD_LIBRARY_PATH="$libpy:$py3path/lib:$py3path/lib64:$LD_LIBRARY_PATH"
-libpydir=`dirname $libpy`
-libpybase=`basename $libpy`
-LDFLAGS="-L$libpydir -l$libpybase" CFLAGS="-I$py3path/include/python${TRAVIS_PYTHON_VERSION}m" ./configure \
+./configure \
     --with-python-path=$py2path \
     --with-python3-path=$py3path
 

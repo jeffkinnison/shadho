78aa699e6e5c249adb63844812991e8cb9362e50
Jeff Kinnison <jeffdkinnison@gmail.com>
2017-07-10 16:29:42 -0400

    fixed complexity to work with the domain of a space rather than the Space backend object

--- shadho/heuristics/complexity.py
+++ shadho/heuristics/complexity.py
@@ -8,11 +8,11 @@ import numpy
 
 @heuristic_check
 def complexity(space):
-    if isinstance(space.domain, scipy.stats.rv_continuous):
+    if isinstance(space, scipy.stats.rv_continuous):
         a, b = space.domain.interval(0.9999)
         d = np.linalg.norm(b - a)
         return 2 + d
-    elif isinstance(space.domain, list):
-        return 2 - (1 / len(space.domain))
+    elif isinstance(space, list):
+        return 2 - (1 / len(space))
     else:
         return 1

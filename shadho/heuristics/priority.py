099f40b2c6dc77156a917c40bfd1b7ddcd1085f6
Jeff Kinnison <jeffdkinnison@gmail.com>
2017-07-11 11:30:59 -0400

    reworked priority.priority to operate on numpy arrays instead of backend classes

--- shadho/heuristics/priority.py
+++ shadho/heuristics/priority.py
@@ -8,24 +8,17 @@ from sklearn.gaussian_process.kernels import RBF
 
 
 @heuristic_check
-def priority(tree):
-    results = np.zeros((1, len(tree.results)), dtype=np.float32)
-    values = None
-    for result in tree.results:
-        if values is None:
-            values = np.zeros((len(results, len(result.values))))
-        results.append(result.loss)
-        values.append([val.value for val in result.values])
-
-    train_len = int(np.ceil(len(results) * 0.8))
-    scales = np.zeros((1, 50))
+def priority(feats, losses):
+    if losses.ndim == 1:
+        losses = losses.reshape((-1, 1))
+    train_len = int(np.ceil(len(feats) * 0.8))
+    scales = np.zeros((1, 50), dtype=np.float64)
     for i in range(50):
-        idx = np.random.permutation(np.arange(len(results)))
+        idx = np.random.permutation(np.arange(len(feats)))
         ls = np.random.uniform(low=0.1, high=2.0)
         kernel = RBF(length_scale=ls)
         gp = GaussianProcessRegressor(kernel=kernel, normalize_y=True)
-        gp.fit(values[idx[:train_len]],
-               results[idx[:train_len]].reshape((-1, 1)))
+        gp.fit(feats[idx[:train_len]], losses[idx[:train_len]])
         scales[i] += (1.0 / gp.kernel.theta[0])
 
     return np.linalg.norm(np.max(scales) - np.min(scales))

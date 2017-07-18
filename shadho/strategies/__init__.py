d93174fa89ee0b68bf0e7fd3b9830dc91630b39e
Jeff Kinnison <jeffdkinnison@gmail.com>
2017-07-18 14:23:04 -0400

    add random search strategy

---
+++ shadho/strategies/__init__.py
@@ -0,0 +1,4 @@
+from .next_value import next_value
+from .random_search import random_search
+
+__all__ = [next_value, random_search]
--- shadho/strategies/random_search.py
+++ shadho/strategies/random_search.py
@@ -1,14 +1,6 @@
 """
 """
 
-import scipy.stats.rv_continuous
-import scipy.stats.randint
 
-
-def random(space):
-    if isinstance(space.domain, scipy.stats.rv_continuous):
-        return space.domain.rvs()
-    elif isinstance(space.domain, list):
-        return space.domain[scipy.stats.randint.rvs(0, len(space.domain))]
-    else:
-        return space.domain
+def random_search(domain):
+    return domain.rvs() if hasattr(domain, 'rvs') else domain

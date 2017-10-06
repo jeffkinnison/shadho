43f287e8f0aaffdaaf1510197deda4d8c189a47f
Jeff Kinnison <jkinniso@nd.edu>
2017-10-05 23:21:36 -0400

    fix heuristics bug

--- shadho/heuristics/priority.py
+++ shadho/heuristics/priority.py
@@ -16,6 +16,6 @@ def priority(feats, losses):
         kernel = RBF(length_scale=ls)
         gp = GaussianProcessRegressor(kernel=kernel, normalize_y=True)
         gp.fit(feats[idx[:train_len]], losses[idx[:train_len]])
-        scales[i] += (1.0 / gp.kernel.theta[0])
+        scales[0][i] += (1.0 / gp.kernel.theta[0])
 
     return np.linalg.norm(np.max(scales) - np.min(scales))

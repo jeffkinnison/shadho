ce9acd3a53de50c0976b7a8b2f46b67ca86f56e0
Jeff Kinnison <jeffdkinnison@gmail.com>
2018-02-09 12:12:48 -0500

    fix broken add_domain() and add_result() during initialization

--- shadho/backend/json/model.py
+++ shadho/backend/json/model.py
@@ -1,6 +1,7 @@
 from shadho.backend.base.model import BaseModel
 from shadho.backend.json.domain import Domain
 from shadho.backend.json.result import Result
+from shadho.backend.utils import InvalidObjectError
 
 import uuid
 
@@ -15,26 +16,37 @@ class Model(BaseModel):
         self.rank = rank
         self.domains = []
         self.results = []
-        map(self.add_domain, domains if domains is not None else [])
-        map(self.add_result, results if results is not None else [])
+
+        domains = [domains] if not isinstance(domains, list) else domains
+        results = [results] if not isinstance(results, list) else results
+        for domain in domains:
+            self.add_domain(domain)
+        for result in results:
+            self.add_result(result)
 
     def add_domain(self, domain):
-        if isinstance(domain, Domain):
-            self.domains.append(domain.id)
-        elif isinstance(domain, (int, str)):
-            self.domains.append(domain)
+        if domain is not None:
+            if isinstance(domain, Domain):
+                self.domains.append(domain.id)
+            elif isinstance(domain, (int, str)):
+                self.domains.append(domain)
+            else:
+                raise InvalidObjectError(domain)
 
     def add_result(self, result):
-        if isinstance(result, Result):
-            self.results.append(result.id)
-        elif isinstance(result, (int, str)):
-            self.results.append(result)
+        if result is not None:
+            if isinstance(result, Result):
+                self.results.append(result.id)
+            elif isinstance(result, (int, str)):
+                self.results.append(result)
+            else:
+                raise InvalidObjectError(result)
 
     def to_json(self):
         return {
             'priority': self.priority,
             'complexity': self.complexity,
             'rank': self.rank,
-            'domains': self.spaces,
+            'domains': self.domains,
             'results': self.results,
         }

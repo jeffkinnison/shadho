9dbfceb1ba01f71d782afaa78999c5adf226b901
Jeff Kinnison <jeffdkinnison@gmail.com>
2018-02-09 12:14:19 -0500

    remove compute_complexity--funcitonality moved to BaseBackend

--- shadho/backend/base/model.py
+++ shadho/backend/base/model.py
@@ -14,29 +14,6 @@ class BaseModel(object):
 
     __tablename__ = 'models'
 
-    def calculate_complexity(self):
-        """Calculate the complexity of the search respresented by this model.
-
-        The complexity is an approximation of the size of the search as the sum
-        of the size of each degree of freedom (leaf node). Trees with a higher
-        complexity score will be allocated more searches on higher-performing
-        hardware or a greater number of searches in the case that hardware is
-        not specified.
-
-        Notes
-        -----
-        The complexity is stored internally in each instance of the Tree class.
-
-        See Also
-        --------
-        `shadho.heuristics.priority`
-        """
-        c = 0.0
-        for domain in self.domains:
-            c += domain.complexity
-
-        self.complexity = c
-
     def add_domain(self, domain):
         """Add a hyperparameter domain to the model.
 

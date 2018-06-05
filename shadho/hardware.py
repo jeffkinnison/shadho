3138a0cb553d13a505853f292521ccf43d00f68a
camicarballo <camigcarballo@gmail.com>
2018-06-05 17:06:20 -0400

    update ComputeClass: ModelGroup, generate(), add/remove_model()

--- shadho/hardware.py
+++ shadho/hardware.py
@@ -1,7 +1,7 @@
 """
 """
 import uuid
-
+from pyrameter import ModelGroup
 
 class ComputeClass(object):
     def __init__(self, name, resource, value, max_tasks):
@@ -12,5 +12,17 @@ class ComputeClass(object):
         self.max_tasks = max_tasks
         self.current_tasks = 0
 
+        self.model_group = ModelGroup()
+
     def __hash__(self):
         return hash((self.id, self.name, self.resource, self.value))
+
+    def generate(self, model_id):
+        return self.model_group.generate(model_id)
+
+    def add_model(self, model):
+        self.model_group.add_model(model)
+
+    def remove_model(self, model_id):
+        self.model_group.remove_model(model_id)
+

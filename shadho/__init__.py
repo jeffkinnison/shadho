aa128894e9652d87b5bb935c8a7ff234460be5bb
Jeff Kinnison <jeffdkinnison@gmail.com>
2019-03-10 15:05:50 -0400

    update to add a global config object

--- shadho/__init__.py
+++ shadho/__init__.py
@@ -1,5 +1,20 @@
-"""
-"""
-from .shadho import Shadho
+"""Top-level imports for SHADHO.
 
-__all__ = ['backend', 'heuristics', 'managers', 'strategies']
+At the top level, the SHADHO package should grant access to the `Shadho` object
+that drives hyperparameter optimization and the configuration.
+
+See Also
+--------
+shadho.shadho
+    Main driver for hyperparameter optimization.
+shadho.configuration
+    Utility for configuring SHADHO.
+
+"""
+
+from .shadho import Shadho
+from .configuration import ShadhoConfig
+
+config = ShadhoConfig()
+
+__all__ = ['Shadho', 'config', 'managers', 'spaces']
